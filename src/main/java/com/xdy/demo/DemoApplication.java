504f28f33378be6fa0ec94369736a2a18c4a46b8
admin <admin@LAPTOP-6SU6L9NI>
2020-06-19 11:08:40 +0800

    修改

---
+++ src/main/java/com/xdy/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.xdy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String test() {
+		return "测试";
+	}
+    @RequestMapping("/test")
+    public String loginOut() {
+        return "退出成功！";
+    }
+}
