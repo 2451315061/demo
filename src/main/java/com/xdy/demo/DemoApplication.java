3f72b6f78ac9a452d8a9ea7b5b0ffa9cb71819cb
admin <admin@LAPTOP-6SU6L9NI>
2020-06-22 09:01:14 +0800

    修改测试

--- src/main/java/com/xdy/demo/DemoApplication.java
+++ src/main/java/com/xdy/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String test() {
-		return "测试8088";
+		return "测试8088--修改";
 	}
     @RequestMapping("/test")
     public String loginOut() {
